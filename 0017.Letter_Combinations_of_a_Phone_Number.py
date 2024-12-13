39d3624022cf3dea043be24120ca6de20b080b53
HIDDENUSER <darkresearchlab@gmail.com>
2024-12-13 23:50:29 +0000

    Update and rename 0017. to 0017.Letter_Combinations_of_a_Phone_Number.py

---
+++ 0017.Letter_Combinations_of_a_Phone_Number.py
@@ -0,0 +1,22 @@
+class Solution:
+    def letterCombinations(self, digits):
+        if not digits:
+            return []
+        phone_map = {
+            '2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
+            '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'
+        }
+        
+        def backtrack(index, path):
+            if index == len(digits):
+                combinations.append("".join(path))
+                return
+            possible_letters = phone_map[digits[index]]
+            for letter in possible_letters:
+                path.append(letter)
+                backtrack(index + 1, path)
+                path.pop()  # Backtrack
+        
+        combinations = []
+        backtrack(0, [])
+        return combinations

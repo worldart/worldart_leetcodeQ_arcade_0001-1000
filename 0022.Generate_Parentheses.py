8de9a91f764f868690d4ad1d66a332b18d5d24d3
HIDDENUSER <darkresearchlab@gmail.com>
2024-12-22 22:23:27 +0000

    Update 0022.Generate_Parentheses.py

--- 0022.Generate_Parentheses.py
+++ 0022.Generate_Parentheses.py
@@ -1,1 +1,28 @@
+# Recursion with Backtracking
+class Solution(object):
+    def generateParenthesis(self, n):
+        
+		def paren(left, right, curr, res):
+			# 'evaluate current string
+			# if we are out of brackets to add, we must be at a valid string
+			if left == 0 and right == 0:
+				res.append(curr)
+				return
 
+			# recursive call: add either open or close
+			# if adding open bracket is valid
+			if left > 0:
+				# add open bracket, decr count
+				paren(left-1, right, curr + "(", res)
+
+			# if adding close bracket is valid
+			if right > left:
+				# add close bracket, decr count
+				paren(left, right-1, curr + ")", res)
+
+			return res
+		# end paren()
+
+		res = paren(n, n, '', [])
+
+		return res

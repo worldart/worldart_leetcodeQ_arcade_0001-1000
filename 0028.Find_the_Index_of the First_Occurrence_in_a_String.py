93c5a5d11daeb4f676d8ed7f9db730710bd7bb08
HIDDENUSER <darkresearchlab@gmail.com>
2024-12-31 02:31:47 +0000

    Create 0028.Find_the_Index_of the First_Occurrence_in_a_String.py

---
+++ 0028.Find_the_Index_of the First_Occurrence_in_a_String.py
@@ -0,0 +1,12 @@
+#6.61% run time
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        # makes sure we don't iterate through a substring that is shorter than needle
+        for i in range(len(haystack) - len(needle) + 1):
+            # check if any substring of haystack with the same length as needle is equal to needle
+            if haystack[i : i+len(needle)] == needle:
+                # if yes, we return the first index of that substring
+                return i
+        # if we exit the loop, return -1        
+        return -1

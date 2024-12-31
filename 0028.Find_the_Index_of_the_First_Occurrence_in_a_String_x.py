3c1996b07a06574d0e7f0f28f64ff8a98aa91c7c
HIDDENUSER <darkresearchlab@gmail.com>
2024-12-31 02:42:03 +0000

    Create 0028.Find_the_Index_of_the_First_Occurrence_in_a_String_x.py

---
+++ 0028.Find_the_Index_of_the_First_Occurrence_in_a_String_x.py
@@ -0,0 +1,10 @@
+#100% run time
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        return haystack.find(needle)

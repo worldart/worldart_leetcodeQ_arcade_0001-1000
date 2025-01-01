c6351b4cca1a690ea35b87a33ebb819d879c81ab
HIDDENUSER <darkresearchlab@gmail.com>
2025-01-01 21:26:18 +0000

    Create 0032.Longest_Valid_Parentheses_python3_x.py

---
+++ 0032.Longest_Valid_Parentheses_python3_x.py
@@ -0,0 +1,15 @@
+#81.74% run time
+
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        stack=[]
+        l=['0']*len(s)
+        for ind,i in enumerate(s):
+            if i=='(':
+                stack.append(ind)
+            else:
+                if stack:
+                    l[stack.pop()]='1'
+                    l[ind]='1'
+        return max(len(i) for i in ''.join(l).split('0'))
+        

376a954d298a7d511678ba5a4e65f82d84092817
HIDDENUSER <darkresearchlab@gmail.com>
2025-01-17 17:11:00 +0000

    Create 0042.Trapping_Rain_Water.cs

---
+++ 0042.Trapping_Rain_Water.cs
@@ -0,0 +1,29 @@
+//100%RT
+
+public class Solution {
+    public int Trap(int[] height) {
+        int total = 0, l = 0, r = height.Length - 1; // Initialize total and two pointers
+        int lmax = 0, rmax = height[r]; // Initialize max heights for left and right
+        
+        while (l < r) {
+            if (height[l] <= height[r]) {
+                // If left height is less than or equal to right height
+                if (height[l] < lmax) {
+                    total += lmax - height[l]; // Water trapped on the left
+                } else {
+                    lmax = height[l]; // Update left max height
+                }
+                l++; // Move left pointer
+            } else {
+                // If right height is less than left height
+                if (height[r] < rmax) {
+                    total += rmax - height[r]; // Water trapped on the right
+                } else {
+                    rmax = height[r]; // Update right max height
+                }
+                r--; // Move right pointer
+            }
+        }
+        return total; // Return total water trapped
+    }
+}

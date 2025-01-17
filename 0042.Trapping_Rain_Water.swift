522dd7f7012790e4358afc088801692b2f535133
HIDDENUSER <darkresearchlab@gmail.com>
2025-01-17 17:17:50 +0000

    Create 0042.Trapping_Rain_Water.swift

---
+++ 0042.Trapping_Rain_Water.swift
@@ -0,0 +1,32 @@
+//100%RT
+
+class Solution {
+    func trap(_ height: [Int]) -> Int {
+        var total = 0 // Initialize total water trapped
+        var l = 0 // Left pointer
+        var r = height.count - 1 // Right pointer
+        var lmax = 0 // Max height on the left
+        var rmax = height[r] // Max height on the right
+        
+        while l < r {
+            if height[l] <= height[r] {
+                // If left height is less than or equal to right height
+                if height[l] < lmax {
+                    total += lmax - height[l] // Water trapped on the left
+                } else {
+                    lmax = height[l] // Update left max height
+                }
+                l += 1 // Move left pointer
+            } else {
+                // If right height is less than left height
+                if height[r] < rmax {
+                    total += rmax - height[r] // Water trapped on the right
+                } else {
+                    rmax = height[r] // Update right max height
+                }
+                r -= 1 // Move right pointer
+            }
+        }
+        return total // Return total water trapped
+    }
+}

1993aa87b2d52b9061b82aa3cd28767b416a45ff
HIDDENUSER <darkresearchlab@gmail.com>
2025-01-17 16:45:09 +0000

    Create 0042.Trapping_Rain_Water.js

---
+++ 0042.Trapping_Rain_Water.js
@@ -0,0 +1,28 @@
+//100%RT
+
+var trap = function(height) {
+    let total = 0; // Initialize total water trapped
+    let l = 0, r = height.length - 1; // Two pointers, left (l) and right (r)
+    let lmax = 0, rmax = height[r]; // Initialize max heights for left and right
+    
+    while (l < r) {
+        if (height[l] <= height[r]) {
+            // If left height is less than or equal to right height
+            if (height[l] < lmax) {
+                total += lmax - height[l]; // Water trapped on the left
+            } else {
+                lmax = height[l]; // Update left max height
+            }
+            l++; // Move left pointer
+        } else {
+            // If right height is less than left height
+            if (height[r] < rmax) {
+                total += rmax - height[r]; // Water trapped on the right
+            } else {
+                rmax = height[r]; // Update right max height
+            }
+            r--; // Move right pointer
+        }
+    }
+    return total; // Return total water trapped
+};

06df84f9f1d861db670679f9f71dfc86ec7a5205
HIDDENUSER <darkresearchlab@gmail.com>
2025-01-21 09:53:06 +0000

    Create 0045.Jump_Game_II.java

---
+++ 0045.Jump_Game_II.java
@@ -0,0 +1,18 @@
+//99.37%RT
+
+
+class Solution {
+    public int jump(int[] nums) {
+        int n = nums.length;
+        int jumps = 0, currentEnd = 0, farthest = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;
+            }
+        }
+        return jumps;
+    }
+}

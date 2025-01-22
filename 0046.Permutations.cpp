6ddf74d8d97f11c2620d4f802a19544db51994f5
HIDDENUSER <darkresearchlab@gmail.com>
2025-01-22 09:10:36 +0000

    Create 0046.Permutations.cpp

---
+++ 0046.Permutations.cpp
@@ -0,0 +1,14 @@
+//100%RT
+
+
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        do {
+            ans.push_back(nums);
+        } while(next_permutation(nums.begin(), nums.end()));
+        return ans;
+    }
+};

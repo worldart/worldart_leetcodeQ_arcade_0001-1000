b1413656154fcf663a10dfc887ef025f1232bf38
HIDDENUSER <darkresearchlab@gmail.com>
2025-02-01 13:40:11 +0000

    Create 0053.Maximum_Subarray_python3.py

---
+++ 0053.Maximum_Subarray_python3.py
@@ -0,0 +1,16 @@
+#93.30%
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        med=0
+        msf=-99999
+        for i in range(0,len(nums)):
+            med=med+nums[i]
+            if(med<nums[i]):
+                med=nums[i]
+            if(msf<med):
+                msf=med
+        return msf
+        
+
+        

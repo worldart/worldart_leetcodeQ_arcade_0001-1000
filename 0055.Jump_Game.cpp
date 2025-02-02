134fdb2f7d3d79f3041b5f81c05df0190cec20ac
HIDDENUSER <darkresearchlab@gmail.com>
2025-02-02 23:33:40 +0000

    Create 0055.Jump_Game.cpp

---
+++ 0055.Jump_Game.cpp
@@ -0,0 +1,14 @@
+//100%RT
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int goal = nums.size() - 1;
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            if (i + nums[i] >= goal) {
+                goal = i;
+            }
+        }
+        return goal == 0;
+    }
+};

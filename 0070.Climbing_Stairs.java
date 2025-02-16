bad1965433145adbe3d3d8c506e9bece8a968fb1
HIDDENUSER <darkresearchlab@gmail.com>
2025-02-16 20:14:54 +0000

    Create 0070.Climbing_Stairs.java

---
+++ 0070.Climbing_Stairs.java
@@ -0,0 +1,14 @@
+//0ms
+
+class Solution {
+    public int climbStairs(int n) {
+        int secondLast = 0;
+        int last = 1;
+        for (int i = 1; i <= n; i++) {
+            int temp = last;
+            last = secondLast + last;
+            secondLast = temp;
+        }
+        return last;
+    }
+}

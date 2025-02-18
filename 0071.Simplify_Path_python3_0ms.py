2a080d23ff3ed820f9a227eaa6a92c4f758c4599
HIDDENUSER <darkresearchlab@gmail.com>
2025-02-18 16:20:53 +0000

    Create 0071.Simplify_Path_python3_0ms.py

---
+++ 0071.Simplify_Path_python3_0ms.py
@@ -0,0 +1,15 @@
+#0ms
+
+
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stk = []
+        for s in path.split('/'):
+            if not s or s == '.':
+                continue
+            if s == '..':
+                if stk:
+                    stk.pop()
+            else:
+                stk.append(s)
+        return '/' + '/'.join(stk)

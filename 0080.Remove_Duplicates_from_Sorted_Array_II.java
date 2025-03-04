9a4b53f1253279ac24c7494012ed8fe204789778
HIDDENUSER <darkresearchlab@gmail.com>
2025-03-04 05:46:01 +0000

    Update 0080.Remove_Duplicates_from_Sorted_Array_II.java

--- 0080.Remove_Duplicates_from_Sorted_Array_II.java
+++ 0080.Remove_Duplicates_from_Sorted_Array_II.java
@@ -17,3 +17,20 @@ class Solution {
         return j;
     }
 }
+
+
+/*
+//0ms
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (j == 1 || nums[i] != nums[j - 2]) {
+                nums[j++] = nums[i];
+            }
+        }
+        return j;
+    }
+}
+*/

60b82752d0f1b2b0e567cde32dbaf3e4b35d1a86
HIDDENUSER <darkresearchlab@gmail.com>
2025-03-10 21:33:42 +0000

    Create 0086.Partition_List.rs

---
+++ 0086.Partition_List.rs
@@ -0,0 +1,41 @@
+//0ms
+
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn partition(mut head: Option<Box<ListNode>>, x: i32) -> Option<Box<ListNode>> {
+        let mut before = ListNode::new(0);
+        let mut after = ListNode::new(0);
+        let mut before_tail = &mut before;
+        let mut after_tail = &mut after;
+
+        while let Some(mut node) = head {
+            head = node.next.take();
+            if node.val < x {
+                before_tail.next = Some(node);
+                before_tail = before_tail.next.as_mut().unwrap();
+            } else {
+                after_tail.next = Some(node);
+                after_tail = after_tail.next.as_mut().unwrap();
+            }
+        }
+
+        before_tail.next = after.next.take();
+
+        before.next
+    }
+}

c82c4e0d048928d58405fac67e2af71854344a91
HIDDENUSER <darkresearchlab@gmail.com>
2025-03-12 14:54:56 +0000

    Create 0088.Merge_Sorted_Array.py

---
+++ 0088.Merge_Sorted_Array.py
@@ -0,0 +1,14 @@
+#0ms
+
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+      for j in range(n):
+          nums1[m+j] = nums2[j]
+      nums1.sort()
+"""
+:type nums1: List[int]
+:type m: int
+:type nums2: List[int]
+:type n: int
+:rtype: None Do not return anything, modify nums1 in-place instead.
+"""

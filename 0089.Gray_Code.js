a76d72113bb9f02cb8f475f0d807014fbad161b7
HIDDENUSER <darkresearchlab@gmail.com>
2025-03-13 19:30:59 +0000

    Create 0089.Gray_Code.js

---
+++ 0089.Gray_Code.js
@@ -0,0 +1,17 @@
+//0-3ms
+
+
+
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var grayCode = function(n) {
+   let result = [];
+    
+    for (let i = 0; i < (1 << n); i++) {
+        result.push(i ^ (i >> 1)); 
+    }
+    
+    return result; 
+};

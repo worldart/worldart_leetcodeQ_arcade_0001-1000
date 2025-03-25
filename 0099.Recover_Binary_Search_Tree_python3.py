64878b04f489ac597cce61d7c22222c94ef1cf41
HIDDENUSER <darkresearchlab@gmail.com>
2025-03-25 20:33:52 +0000

    Create 0099.Recover_Binary_Search_Tree_python3.py

---
+++ 0099.Recover_Binary_Search_Tree_python3.py
@@ -0,0 +1,45 @@
+#0ms
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        def recoverTree(self, root: Optional[TreeNode]) -> None:
+        
+            res = []     
+            startnode = None
+            prev = None
+            lastnode = None
+            
+            def dfs(root):
+                nonlocal res, startnode, prev, lastnode
+                if not root:
+                    return 
+                # go to left  (inorder step 1)  
+                dfs(root.left)
+                
+                # do processing....(inorder step 2)
+                # get the first node where the sorted order is broken the first time and the last time
+                if prev and prev.val > root.val:
+                    if not startnode:
+                        startnode = prev
+                    lastnode = root
+                    
+                prev = root
+                
+                # go to right (inorder step 3)    
+                dfs(root.right)
+                
+            
+            dfs(root)
+            # swap the nodes that are not in place
+            if startnode and lastnode:
+                startnode.val, lastnode.val = lastnode.val, startnode.val

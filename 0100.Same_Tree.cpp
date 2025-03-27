67b3ff77d55779017eeee0774bd8ba46d4f33fbf
HIDDENUSER <darkresearchlab@gmail.com>
2025-03-27 03:51:37 +0000

    Update 0100.Same_Tree.cpp

--- 0100.Same_Tree.cpp
+++ 0100.Same_Tree.cpp
@@ -22,3 +22,17 @@ public:
         return isSameTree(p->left,q->left)&&isSameTree(p->right,q->right);
     }
 };
+
+
+/*
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(!p && !q)    return true;
+        if(!p || !q)    return false;
+        return p->val==q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
+
+
+*/

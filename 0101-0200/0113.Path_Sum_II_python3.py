c8dca9dcc4d00851f17830f0b3db0217d4e52ceb
HIDDENUSER <darkresearchlab@gmail.com>
2025-04-13 00:38:10 +0100

    Create 0113.Path_Sum_II_python3.py

---
+++ 0101-0200/0113.Path_Sum_II_python3.py
@@ -0,0 +1,26 @@
+#0ms
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: TreeNode, targetSum: int) -> List[List[int]]:
+        def dfs(root, targetSum, path):
+            if root == None: return None
+            targetSum -= root.val
+            path.append(root.val)
+            if root.left == None and root.right == None:  # Is leaf node
+                if targetSum == 0:  # Found a valid path
+                    ans.append(path.copy())
+            else:
+                dfs(root.left, targetSum, path)
+                dfs(root.right, targetSum, path)
+            path.pop()  # backtrack
+
+        ans = []
+        dfs(root, targetSum, [])
+        return ans

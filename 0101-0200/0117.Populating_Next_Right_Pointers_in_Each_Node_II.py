fa8636ec4f9e5f0ca082692651cddaa041b145a9
HIDDENUSER <darkresearchlab@gmail.com>
2025-04-19 23:27:30 +0100

    Create 0117.Populating_Next_Right_Pointers_in_Each_Node_II.py

---
+++ 0101-0200/0117.Populating_Next_Right_Pointers_in_Each_Node_II.py
@@ -0,0 +1,31 @@
+#22ms
+
+
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=0, left=None, right=None, next=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+        :type root: Node
+        :rtype: Node
+"""
+
+class Solution:
+    def connect(self, root):
+        node = root
+        while node:
+            curr = dummy = Node(0)
+            while node:
+                if node.left:
+                    curr.next = node.left
+                    curr = curr.next
+                if node.right:
+                    curr.next = node.right
+                    curr = curr.next
+                node = node.next
+            node = dummy.next
+               
+        return root

acce78d451b9b8a7ce9d26ec435f7fd49b4c2954
HIDDENUSER <darkresearchlab@gmail.com>
2025-04-27 20:25:48 +0100

    Create 0121.Best_Time_to_Buy_and_Sell_Stock.java

---
+++ 0101-0200/0121.Best_Time_to_Buy_and_Sell_Stock.java
@@ -0,0 +1,28 @@
+//1ms
+
+
+ class Solution {
+    static {
+        for(int i = 0; i<500; i++)
+        {
+            maxProfit(new int[] {});
+        }
+    }
+    public static int maxProfit(int[] a) {
+        int least = Integer.MAX_VALUE;
+        int max = 0 ;
+        for(int i = 0 ; i<a.length; i++)
+        {
+            int profit = a[i]-least;
+            if(profit>=0)
+            {
+                max = (profit > max)? profit : max;
+            }
+            else
+            {
+                least = a[i];
+            }
+        }
+        return max;
+    }
+}

dd385046a450670a5ddabb7072f697c96604c9dc
HIDDENUSER <darkresearchlab@gmail.com>
2025-05-17 20:44:47 +0100

    Update and rename 0133. to 0133.Clone_Graph.cpp

---
+++ 0101-0200/0133.Clone_Graph.cpp
@@ -0,0 +1,178 @@
+//8ms
+
+
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+
+class Solution {
+    public:
+    Node* dfs(Node* cur,unordered_map<Node*,Node*>& mp)
+    {
+        vector<Node*> neighbour;
+        Node* clone=new Node(cur->val);
+        mp[cur]=clone;
+            for(auto it:cur->neighbors)
+            {
+                if(mp.find(it)!=mp.end())   //already clone and stored in map
+                {
+                    neighbour.push_back(mp[it]);    //directly push back the clone node from map to neigh
+                }
+                else
+                    neighbour.push_back(dfs(it,mp));
+            }
+            clone->neighbors=neighbour;
+            return clone;
+    }
+    Node* cloneGraph(Node* node) {
+        unordered_map<Node*,Node*> mp;
+        if(node==NULL)
+            return NULL;
+        if(node->neighbors.size()==0)   //if only one node present no neighbors
+        {
+            Node* clone= new Node(node->val);
+            return clone; 
+        }
+
+
+
+
+
+
+
+
+
+//7ms
+
+
+
+
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    unordered_map<int, Node*> cloned_nodes;
+
+    Node* cloneGraph(Node* node) 
+    {
+        if (node == nullptr)
+        {
+            return nullptr;
+        }
+
+        if (cloned_nodes.count(node->val))
+        {
+            return cloned_nodes[node->val];
+        }
+
+        Node* cloned_node = new Node(node->val);
+        cloned_nodes[node->val] = cloned_node;
+
+        for (Node* orig_neighbor : node->neighbors)
+        {
+            Node* cloned_neighbor = cloneGraph(orig_neighbor);
+            cloned_node->neighbors.push_back(cloned_neighbor);
+        }
+
+        return cloned_node;
+    }
+};
+        return dfs(node,mp);
+    }
+
+
+
+
+
+
+//3ms
+
+
+
+
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    map<Node*, Node*> vis;
+    void cloneTo(Node* node, Node* mynode){
+        mynode->val = node->val;
+        vis[node] = mynode;
+        for(auto x : node->neighbors){
+            if(vis.count(x)){
+                mynode->neighbors.push_back(vis[x]);
+            }
+            else{
+                auto t = new Node;
+                mynode->neighbors.push_back(t);
+                cloneTo(x,t);
+            }
+        }
+    }
+    Node* cloneGraph(Node* node) {
+        auto mynode = new Node;
+        if(!node) return nullptr;
+        cloneTo(node,mynode);
+        return mynode;
+    }
+};
+};

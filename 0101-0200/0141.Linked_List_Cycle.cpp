6efba18629c3d329539254c3dc0e2b0f55141220
HIDDENUSER <darkresearchlab@gmail.com>
2025-05-26 09:47:10 +0100

    Create 0141.Linked_List_Cycle.cpp

---
+++ 0101-0200/0141.Linked_List_Cycle.cpp
@@ -0,0 +1,67 @@
+//10ms
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) {
+                return true; // Cycle detected
+            }
+        }
+        return false; // No cycle
+    }
+};
+
+
+
+
+//8ms
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while(fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(fast == slow)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+
+
+
+

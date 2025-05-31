33d2b03be1131d3e8b0c804dfc89ba48770e76d8
HIDDENUSER <darkresearchlab@gmail.com>
2025-05-31 15:07:28 +0100

    Create 0143.Reorder_List_python3.py

---
+++ 0101-0200/0143.Reorder_List_python3.py
@@ -0,0 +1,75 @@
+#7ms
+
+
+
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        fast = head
+        slow = head
+
+        # Step 1: Find the middle of the list
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        # Step 2: Reverse the second half of the list
+        second = slow.next
+        slow.next = None
+        node = None
+
+        while second:
+            temp = second.next
+            second.next = node
+            node = second
+            second = temp
+
+        # Step 3: Merge the two halves
+        first = head
+        second = node
+
+        while second:
+            temp1, temp2 = first.next, second.next
+            first.next, second.next = second, temp1
+            first, second = temp1, temp2
+
+
+
+
+#0ms
+
+
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        second = slow.next
+        slow.next = None
+        prev = None
+        while second:
+            temp = second.next
+            second.next = prev
+            prev = second
+            second = temp
+        first, second = head, prev
+        while second:
+            temp1, temp2 = first.next, second.next
+            first.next = second
+            second.next = temp1
+            first = temp1
+            second = temp2
+
+
+        

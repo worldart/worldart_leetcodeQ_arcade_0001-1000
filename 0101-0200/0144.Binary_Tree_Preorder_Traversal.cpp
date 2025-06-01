a7cebf0355e201194846b98a7eae9db913dbec85
HIDDENUSER <darkresearchlab@gmail.com>
2025-06-01 22:10:56 +0100

    Update 0144.Binary_Tree_Preorder_Traversal.cpp

--- 0101-0200/0144.Binary_Tree_Preorder_Traversal.cpp
+++ 0101-0200/0144.Binary_Tree_Preorder_Traversal.cpp
@@ -39,3 +39,41 @@ public:
         return result;
     }
 };
+
+
+
+
+
+//0ms
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
+        dfs(root, result);
+        return result;
+    }
+    
+    void dfs(TreeNode* node, vector<int>& result) {
+        if (!node) return;
+        result.push_back(node->val); // Visit root
+        dfs(node->left, result);     // Traverse left subtree
+        dfs(node->right, result);    // Traverse right subtree
+    }
+};
+

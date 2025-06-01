c159dc18968f624fc2ac5b821ac9be7d6e9ee03e
HIDDENUSER <darkresearchlab@gmail.com>
2025-06-01 22:14:13 +0100

    Update 0144.Binary_Tree_Preorder_Traversal.java

--- 0101-0200/0144.Binary_Tree_Preorder_Traversal.java
+++ 0101-0200/0144.Binary_Tree_Preorder_Traversal.java
@@ -32,3 +32,90 @@ class Solution {
 
     }
 }
+
+
+
+
+//0ms
+
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+import java.util.*;
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        dfs(root, result);
+        return result;
+    }
+    
+    private void dfs(TreeNode node, List<Integer> result) {
+        if (node == null) return;
+        result.add(node.val);       // Visit root
+        dfs(node.left, result);     // Traverse left subtree
+        dfs(node.right, result);    // Traverse right subtree
+    }
+}
+
+
+
+
+
+//0ms
+
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+import java.util.*;
+
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+        
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            result.add(node.val);  // Visit node
+            
+            if (node.right != null) stack.push(node.right);  // Push right child
+            if (node.left != null) stack.push(node.left);    // Push left child
+        }
+        
+        return result;
+    }
+}

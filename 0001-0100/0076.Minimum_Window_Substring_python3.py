0a209d940f6e2fc81abbb751b3f119552fd88201
HIDDENUSER <darkresearchlab@gmail.com>
2025-06-02 16:00:12 +0100

    Update 0076.Minimum_Window_Substring_python3.py

--- 0001-0100/0076.Minimum_Window_Substring_python3.py
+++ 0001-0100/0076.Minimum_Window_Substring_python3.py
@@ -1,7 +1,6 @@
-#0ms
+#121ms
 
 
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
 
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
@@ -39,3 +38,38 @@ class Solution:
                 left = left + 1
 
         return ans[-1]
+
+
+
+
+#39ms
+
+
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(s) < len(t):
+            return ""
+        needstr = collections.defaultdict(int)
+        for ch in t:
+            needstr[ch] += 1
+        needcnt = len(t)
+        res = (0, float('inf'))
+        start = 0
+        for end, ch in enumerate(s):
+            if needstr[ch] > 0:
+                needcnt -= 1
+            needstr[ch] -= 1
+            if needcnt == 0:
+                while True:
+                    tmp = s[start]
+                    if needstr[tmp] == 0:
+                        break
+                    needstr[tmp] += 1
+                    start += 1
+                if end - start < res[1] - res[0]:
+                    res = (start, end)
+                needstr[s[start]] += 1
+                needcnt += 1
+                start += 1
+        return '' if res[1] > len(s) else s[res[0]:res[1]+1]

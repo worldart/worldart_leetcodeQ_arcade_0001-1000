30173d1f0079cd816997122d6d6f88b4b7fa50f2
HIDDENUSER <darkresearchlab@gmail.com>
2025-06-08 20:49:36 +0100

    Update 0147.Insertion_Sort_List.js

--- 0101-0200/0147.Insertion_Sort_List.js
+++ 0101-0200/0147.Insertion_Sort_List.js
@@ -1,1 +1,42 @@
+//36ms
 
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var insertionSortList = function (head) {
+    if (!head || !head.next) return head;
+    const bucket = new ListNode(-Infinity, null);
+
+    function insert(el) {
+        let prev = bucket;
+        while (prev.next && prev.next.val < el.val) {
+            prev = prev.next;
+        }
+        [el.next, prev.next] = [prev.next, el];
+    }
+
+    let node = head;
+    while (node) {
+        const nextNode = node.next;
+        insert(node);
+        node = nextNode;
+    }
+    return bucket.next;
+};

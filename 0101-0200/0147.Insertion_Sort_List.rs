cea074d49b8b302b702d5bd86dff6e1585ab1f78
HIDDENUSER <darkresearchlab@gmail.com>
2025-06-09 01:08:59 +0100

    Create 0147.Insertion_Sort_List.rs

---
+++ 0101-0200/0147.Insertion_Sort_List.rs
@@ -0,0 +1,44 @@
+
+
+
+
+
+
+
+
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn insertion_sort_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut dummy = Box::new(ListNode::new(i32::MIN));
+        let mut curr = head;
+        
+        while let Some(mut node) = curr {
+            curr = node.next.take(); 
+            let mut prev = &mut dummy;
+
+            while prev.next.is_some() && prev.next.as_ref().unwrap().val < node.val {
+                prev = prev.next.as_mut().unwrap();
+            }
+            
+            node.next = prev.next.take();
+            prev.next = Some(node);
+        }
+        
+        return dummy.next
+    }
+}

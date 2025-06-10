cdb414eb58c53a5a5da2ce4adba590377dd72694
HIDDENUSER <darkresearchlab@gmail.com>
2025-06-10 21:54:31 +0100

    Update 0148.Sort_List.cpp

--- 0101-0200/0148.Sort_List.cpp
+++ 0101-0200/0148.Sort_List.cpp
@@ -132,10 +132,63 @@ public:
 
 
 
-//
+//13ms
 
 
 
 
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+ 
+class Solution {
+private:
+    ListNode* middle(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* prev = nullptr;
+        while (fast && fast->next) {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return prev;
+    }
 
+    ListNode* merge(ListNode* head1, ListNode* head2) {
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+        while (head1 && head2) {
+            if (head1->val < head2->val) {
+                tail->next = head1;
+                head1 = head1->next;
+            } else {
+                tail->next = head2;
+                head2 = head2->next;
+            }
+            tail = tail->next;
+        }
+        if (head1) tail->next = head1;
+        if (head2) tail->next = head2;
+        return dummy.next;
+    }
+
+public:
+    ListNode* sortList(ListNode* head) {
+        if (!head || !head->next) return head;
+        ListNode* midPrev = middle(head);
+        ListNode* right = midPrev->next;
+        midPrev->next = nullptr;
+        ListNode* left = sortList(head);
+        right = sortList(right);
+        return merge(left, right);
+    }
+};

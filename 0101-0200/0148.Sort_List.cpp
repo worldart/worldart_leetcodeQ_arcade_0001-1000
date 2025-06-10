c215571be1669de122aaa9d17562fa3f2c0b947c
HIDDENUSER <darkresearchlab@gmail.com>
2025-06-10 21:53:15 +0100

    Update 0148.Sort_List.cpp

--- 0101-0200/0148.Sort_List.cpp
+++ 0101-0200/0148.Sort_List.cpp
@@ -62,3 +62,70 @@ public:
 
 
 //
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        // use merge sort
+        // cut list into two
+        
+        // base case
+        if(head==nullptr || head->next == nullptr) return head;
+
+        ListNode *slow = head;
+        ListNode *fast = head;
+        ListNode *prev;
+        while(fast && fast->next)
+        {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        prev->next = nullptr;
+
+        // recursion
+        ListNode *l1 = sortList(head);
+        ListNode *l2 = sortList(slow);
+
+        return mergeList(l1, l2);
+    }
+
+    ListNode* mergeList(ListNode* l1, ListNode* l2)
+    {
+        ListNode dummy(0);
+        ListNode *curr = &dummy;
+
+        while(l1 && l2)
+        {
+            if(l1->val < l2->val)
+            {
+                curr->next = l1;
+                curr = curr->next;
+                l1 = l1->next;
+            }
+            else
+            {
+                curr->next = l2;
+                curr = curr->next;
+                l2 = l2->next;
+            }
+        }
+
+        if(l1) curr->next = l1;
+        if(l2) curr->next = l2;
+        
+        return dummy.next;
+    }
+};

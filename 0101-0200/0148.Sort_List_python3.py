e54938b4893fe8293ed7222ec677736b66cf5bd5
HIDDENUSER <darkresearchlab@gmail.com>
2025-06-15 17:02:54 +0100

    Update 0148.Sort_List_python3.py

--- 0101-0200/0148.Sort_List_python3.py
+++ 0101-0200/0148.Sort_List_python3.py
@@ -89,3 +89,29 @@ class Solution:
 #
 
 
+
+
+
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        arr = []
+        temp = head
+        while(temp):
+            arr.append(temp.val)
+            temp = temp.next
+        
+        arr.sort()
+        temp = head
+        index = 0
+        while(temp):
+            temp.val = arr[index]
+            index+=1
+            temp = temp.next
+        return head

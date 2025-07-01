cbf52544a61b3ff70616d70b5d7beba84099279b
HIDDENUSER <darkresearchlab@gmail.com>
2025-07-01 21:13:57 +0100

    Update 0148.Sort_List.js

--- 0101-0200/0148.Sort_List.js
+++ 0101-0200/0148.Sort_List.js
@@ -1,4 +1,61 @@
-//
+//23ms
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+function ListNode(val, next = null) {
+    this.val = val;
+    this.next = next;
+}
+
+var sortList = function(head) {
+    if (!head || !head.next) return head;
+
+    const mid = getMid(head);
+    const left = sortList(head);
+    const right = sortList(mid);
+
+    return merge(left, right);
+};
+
+function getMid(head) {
+    let slow = head, fast = head, prev = null;
+    while (fast && fast.next) {
+        prev = slow;
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    prev.next = null;
+    return slow;
+}
+
+function merge(left, right) {
+    const dummy = new ListNode(0);
+    let tail = dummy;
+
+    while (left && right) {
+        if (left.val < right.val) {
+            tail.next = left;
+            left = left.next;
+        } else {
+            tail.next = right;
+            right = right.next;
+        }
+        tail = tail.next;
+    }
+
+    tail.next = left || right;
+    return dummy.next;
+}
 
 
 

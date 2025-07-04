bb4e73a2329c806ea69b16402ca65550b7ae8aa1
HIDDENUSER <darkresearchlab@gmail.com>
2025-07-04 06:51:45 +0100

    Update 0150.Evaluate_Reverse_Polish_Notation.py

--- 0101-0200/0150.Evaluate_Reverse_Polish_Notation.py
+++ 0101-0200/0150.Evaluate_Reverse_Polish_Notation.py
@@ -130,3 +130,33 @@ class Solution:
                 stack.append(int(token))
         
         return stack[0]
+
+
+
+
+
+#0ms
+
+
+
+
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        st = []
+
+        for c in tokens:
+            if c == "+":
+                st.append(st.pop() + st.pop())
+            elif c == "-":
+                second, first = st.pop(), st.pop()
+                st.append(first - second)
+            elif c == "*":
+                st.append(st.pop() * st.pop())
+            elif c == "/":
+                second, first = st.pop(), st.pop()
+                st.append(int(first / second))                
+            else:
+                st.append(int(c))
+        
+        return st[0]

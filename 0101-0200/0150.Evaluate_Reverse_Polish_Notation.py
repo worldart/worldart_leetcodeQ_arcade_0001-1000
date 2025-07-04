5a096ebad1ced21edef018c74f23c3c3e5fd8380
HIDDENUSER <darkresearchlab@gmail.com>
2025-07-04 04:34:55 +0100

    Update 0150.Evaluate_Reverse_Polish_Notation.py

--- 0101-0200/0150.Evaluate_Reverse_Polish_Notation.py
+++ 0101-0200/0150.Evaluate_Reverse_Polish_Notation.py
@@ -33,3 +33,100 @@ class Solution(object):
         
         
         return stack[-1]
+
+
+
+
+$2ms
+
+
+class Solution(object):
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        operators = {"+", "-", "*", "/"}
+        stack = []
+        for token in tokens:
+            if token not in operators:
+                stack.append(int(token))
+                # print(stack)
+            else:
+                num2 = stack.pop()
+                num1 = stack.pop()
+                if token == "+":
+                    new_num = num1 + num2
+                elif token == "-":
+                    new_num = num1 - num2
+                elif token == "*":
+                    new_num = num1 * num2
+                elif token == "/":
+                    new_num = int(float(num1) / num2)
+                stack.append(new_num)
+                # print(stack)
+        return stack[-1]
+
+
+
+
+
+#1ms
+
+
+
+
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        list1=['+','-','*','/']
+        dict1={
+            '+': lambda a,b: int(a)+int(b),
+            '-': lambda a,b: int(a)-int(b),
+            '*': lambda a,b: int(a)*int(b),
+            '/': lambda a,b: int(a/b)
+        }
+        list2=[]
+        for i in range(0,len(tokens)):
+            if tokens[i] in list1:
+                x=list2.pop()
+                y=list2.pop()
+                rst=dict1[tokens[i]](y,x)
+                list2.append(rst)
+            else:
+                list2.append(int(tokens[i]))
+        return list2[0]
+
+
+
+
+
+
+
+#3ms
+
+
+
+
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+
+        for token in tokens:
+            if token in {"+", "-", "*", "/"}:
+                b = stack.pop()
+                a = stack.pop()
+
+                if token == "+":
+                    stack.append(a + b)
+                elif token == "-":
+                    stack.append(a - b)
+                elif token == "*":
+                    stack.append(a * b)
+                elif token == "/":
+                    stack.append(int(a / b))
+            else:
+                stack.append(int(token))
+        
+        return stack[0]

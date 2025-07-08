3bcfee7b715b101fa19f7bbf53043be3cc4acc5c
HIDDENUSER <darkresearchlab@gmail.com>
2025-07-08 10:34:09 +0100

    Create 0152.Maximum_Product_Subarray.py

---
+++ 0101-0200/0152.Maximum_Product_Subarray.py
@@ -0,0 +1,37 @@
+//9ms
+
+
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        curMax, curMin = 1, 1
+        res = nums[0]
+        
+        for n in nums:
+            vals = (n, n * curMax, n * curMin)
+            curMax, curMin = max(vals), min(vals)
+			
+            res = max(res, curMax)
+            
+        return res
+
+
+
+
+
+//7ms
+
+
+
+
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        prefix, suffix, max_so_far = 0, 0, float('-inf')
+        for i in range(len(nums)):
+            prefix = (prefix or 1) * nums[i]
+            suffix = (suffix or 1) * nums[~i]
+            max_so_far = max(max_so_far, prefix, suffix)
+        return max_so_far
+
+        

cf38572a09f8664e1af149f1d82f45fc6184ea38
HIDDENUSER <darkresearchlab@gmail.com>
2025-07-21 21:01:48 +0100

    Update 0154.Find_Minimum_in_Rotated_Sorted_Array_II.cpp

--- 0101-0200/0154.Find_Minimum_in_Rotated_Sorted_Array_II.cpp
+++ 0101-0200/0154.Find_Minimum_in_Rotated_Sorted_Array_II.cpp
@@ -52,3 +52,17 @@ public:
 
 
 
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n = nums.size();
+        int ans = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < nums[i - 1]) {
+                ans = nums[i];
+            }
+        }
+        return ans;
+    }
+};

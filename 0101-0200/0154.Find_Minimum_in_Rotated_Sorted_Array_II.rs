9bd0edca6a33bf3264fc6b791b5ff237142bf247
HIDDENUSER <darkresearchlab@gmail.com>
2025-07-21 21:09:25 +0100

    Update 0154.Find_Minimum_in_Rotated_Sorted_Array_II.rs

--- 0101-0200/0154.Find_Minimum_in_Rotated_Sorted_Array_II.rs
+++ 0101-0200/0154.Find_Minimum_in_Rotated_Sorted_Array_II.rs
@@ -1,1 +1,47 @@
+//0ms
 
+
+
+impl Solution {
+    pub fn find_min(nums: Vec<i32>) -> i32 {
+        let (mut left, mut right): (usize, usize) = (0, nums.len()-1);
+        
+        if nums[left] < nums[right] {
+            return nums[left];
+        }
+        
+        while left < right {
+            let mid = left + (right-left)/2;
+            
+            if nums[mid] > nums[right] {
+                left = mid+1;
+            } else if nums[mid] < nums[right] {
+                right = mid;
+            } else {
+                right-=1;  //avoid duplication
+            }
+        }
+        
+        nums[right]
+    }
+}
+
+
+
+
+
+//0ms
+
+
+
+
+
+impl Solution {
+    pub fn find_min(nums: Vec<i32>) -> i32 {
+        let mut min = i32::MAX;
+        for n in nums {
+            min = min.min(n);
+        }
+        min
+    }
+}

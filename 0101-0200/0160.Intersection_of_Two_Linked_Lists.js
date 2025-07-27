418d8a8f58a79db4be655bf6c4363cea7ca6f501
HIDDENUSER <darkresearchlab@gmail.com>
2025-07-27 22:32:37 +0100

    Create 0160.Intersection_of_Two_Linked_Lists.js

---
+++ 0101-0200/0160.Intersection_of_Two_Linked_Lists.js
@@ -0,0 +1,73 @@
+//50ms
+
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+    if (!headA || !headB) return null;
+    var curA = headA;
+    var curB = headB;
+    while (curA != curB) {
+        curA = curA == null ? headB : curA.next;
+        curB = curB == null ? headA : curB.next;
+    }
+    return curA;
+};
+
+
+
+
+
+
+//53ms
+
+
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+
+    // need to add all headB into set
+    let headBNodes = new Set();
+    while(headB){
+        headBNodes.add(headB);
+        headB = headB.next;
+    }
+
+    // check if nodeA is present in headBNodes
+    while(headA){
+        if(headBNodes.has(headA)){
+            return headA;
+        }
+        headA = headA.next;
+    }
+    return null;    
+};

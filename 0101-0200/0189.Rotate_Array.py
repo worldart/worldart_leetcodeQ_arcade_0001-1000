409dc0ae21ba383f28c8d35c9883eb604721c3e5
HIDDENUSER <darkresearchlab@gmail.com>
2025-11-12 19:23:11 +0000

    Add in-place array rotation method
    Implemented an optimized in-place rotation of the array using reverse operations.

---
+++ 0101-0200/0189.Rotate_Array.py
@@ -0,0 +1,47 @@
+#7ms
+
+
+
+
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k %= n
+
+        def reverse(l, r):
+            while l < r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+                r -= 1
+
+        reverse(0, n - 1)
+        reverse(0, k - 1)
+        reverse(k, n - 1)       
+
+
+
+
+
+
+#0ms
+
+
+
+
+
+
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        k %= len(nums)
+        nums.reverse() 
+        nums[k:] = reversed(nums[k:])
+        nums[:k] = reversed(nums[:k])

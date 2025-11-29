bcb38a157b580a2a663f41a2e5294e02de536c28
HIDDENUSER <darkresearchlab@gmail.com>
2025-11-29 15:41:13 +0000

    Update 0190.Reverse_Bits.cpp
    Implement bit reversal function in Solution class.

--- 0101-0200/0190.Reverse_Bits.cpp
+++ 0101-0200/0190.Reverse_Bits.cpp
@@ -48,10 +48,23 @@ public:
 
 
 
-//
+//0ms
 
 
 
 
 
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans = 0;
 
+        for(int i = 0; i < 32; i++) {
+            ans = ans << 1;      // ans me 1 bit ki jagah bana di
+            ans = ans | (n & 1); // n ka last bit ans me daal diya
+            n = n >> 1;          // n ka last bit hata diya
+        }
+
+        return ans;
+    }
+};

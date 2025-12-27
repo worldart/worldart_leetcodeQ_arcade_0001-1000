df017ffa4a722d9a540f204989049eb5d282497c
HIDDENUSER <darkresearchlab@gmail.com>
2025-12-27 09:34:24 +0000

    Refactor delete_duplicate_emails function
    Refactor delete_duplicate_emails function for clarity.

---
+++ 0101-0200/0196.Delete_Duplicate_Emails.py
@@ -0,0 +1,31 @@
+//263ms
+
+
+
+
+import pandas as pd
+
+def delete_duplicate_emails(person: pd.DataFrame) -> None:
+    # Step 1: Sort by id (ascending order)
+    person.sort_values(by='id', inplace=True)
+
+    # Step 2: Drop duplicates based on email column (keep first occurrence)
+    person.drop_duplicates(subset=['email'], inplace=True)
+
+
+
+
+
+
+//262ms
+
+
+
+
+
+
+import pandas as pd
+
+def delete_duplicate_emails(person: pd.DataFrame) -> None:
+    person.sort_values('id' , inplace = True)
+    person.drop_duplicates(subset=['email'], keep = 'first', inplace = True)

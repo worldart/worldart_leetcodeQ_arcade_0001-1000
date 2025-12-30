4e1474b8fc7394fb9881bc07e9d31550a186ef5b
HIDDENUSER <darkresearchlab@gmail.com>
2025-12-30 10:33:48 +0000

    Implement dynamic programming solution for House Robber

--- 0101-0200/0198.House_Robber.cpp
+++ 0101-0200/0198.House_Robber.cpp
@@ -26,3 +26,34 @@ public:
         return dp[n - 1];        
     }
 };
+
+
+
+
+
+
+//0ms
+
+
+
+
+
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int size = nums.size();
+        if (size == 0) {
+            return 0;
+        }
+        vector<int> dp(size + 1);
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        for (int i = 2; i <= size; i++) {
+            dp[i] = max(nums[i - 1] + dp[i - 2], dp[i - 1]);
+        }
+
+        return dp[size];
+    }
+};

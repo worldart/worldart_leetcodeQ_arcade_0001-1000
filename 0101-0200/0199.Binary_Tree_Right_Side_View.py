cf03d41237d901e51eed18f20fb7b8dc927a558c
HIDDENUSER <darkresearchlab@gmail.com>
2026-01-13 11:26:58 +0000

    Create 0199.Binary_Tree_Right_Side_View.py

---
+++ 0101-0200/0199.Binary_Tree_Right_Side_View.py
@@ -0,0 +1,64 @@
+#0ms
+
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+
+        q = deque()
+        q.append(root)
+
+        while q:
+            right_side = None
+
+            for _ in range(len(q)):
+                node = q.popleft()
+                if node:
+                    right_side = node
+                    q.append(node.left)
+                    q.append(node.right)
+            
+            if right_side:
+                res.append(right_side.val)
+        
+        return res
+
+
+
+
+
+
+
+#0ms
+
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        s=[]
+        level=0
+        def bfs(root,level):
+            if root is None:
+                return False
+            if level==len(s):
+                s.append(root.val)
+            bfs(root.right,level+1)
+            bfs(root.left,level+1)
+        bfs(root,0)
+        return s
+        

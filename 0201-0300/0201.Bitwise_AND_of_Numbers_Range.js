c4d543514a41b7fd2093a926947d72a26df5bbdb
HIDDENUSER <darkresearchlab@gmail.com>
2026-02-05 04:19:04 +0000

    Refactor rangeBitwiseAnd implementation
    Refactor rangeBitwiseAnd function for clarity and efficiency.

--- 0201-0300/0201.Bitwise_AND_of_Numbers_Range.js
+++ 0201-0300/0201.Bitwise_AND_of_Numbers_Range.js
@@ -1,1 +1,49 @@
+//6ms
 
+
+
+
+
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+var rangeBitwiseAnd = function(left, right) {
+    // Calculate the common leftmost bits between left and right
+    let shift = 0;
+    while (left < right) {
+        // Right shift both left and right to find common bits
+        left >>= 1;
+        right >>= 1;
+        shift++;
+    }
+    // Left shift the common bits to the original position
+    return left << shift;
+};
+
+
+
+
+
+//3ms
+
+
+
+
+
+
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+var rangeBitwiseAnd = function(left, right) {
+    let shift_count = 0
+    while (left != right) {
+        left = left >> 1
+        right = right >> 1
+        shift_count++
+    }
+    return left << shift_count
+};

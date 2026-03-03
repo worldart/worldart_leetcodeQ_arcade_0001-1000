1393c88b2ba7dc3180de25a38ceb80cbd6907642
HIDDENUSER <darkresearchlab@gmail.com>
2026-03-03 01:51:12 +0000

    Add removeElements method to linked list solution
    Implemented a new method to remove elements from a linked list using a dummy node for better handling of head removals.

---
+++ 0201-0300/0203.Remove_Linked_List_Elements.java
@@ -0,0 +1,84 @@
+//1ms
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+public ListNode removeElements(ListNode head, int val) {
+        if (head == null) return null;
+        head.next = removeElements(head.next, val);
+        return head.val == val ? head.next : head;
+}
+}
+
+
+
+
+
+//1ms
+
+
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+       ListNode dummy=new ListNode(0);
+       dummy.next=head;
+       ListNode prev=dummy;
+        ListNode curr=head;
+        while(curr!=null){
+            if(curr.val==val){
+                prev.next=curr.next;
+            }
+            else{
+                prev=curr;
+            }
+            curr=curr.next;
+        }
+        return dummy.next;
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
